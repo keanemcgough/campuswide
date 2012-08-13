5249a4b02960326bf65c15c9e0017e60c34f600b
Keane <keanemcgough@gmail.com>
2012-08-13 13:33:52 -0500

    added files

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="main.css" />
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <table id="ta">
+
+            <tr>
+                <td id="top" colspan="3">
+                    Campus Wide
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td id="tabs" >
+                     <?php
+                        echo '<a class="tab" href=#> Classes <a> &nbsp; ';
+                        echo '<a class="tab" href=#2> Finaces   <a>  &nbsp; &nbsp; ';
+                        echo '<a class="tab" href=#3> Grades  <a> &nbsp; &nbsp; ';
+                        echo '<a class="tab" href=#4> Admin <a> &nbsp; ';
+                     ?>  
+                </td>
+                <td></td>
+            </tr>
+            <tr width="100%">
+
+                <td id="left">
+                     <?php
+                        echo "left";
+                     ?>  
+                </td>
+        
+           
+                <td id="middle">
+                     <?php
+                         echo "middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle middle ";
+                     ?>  
+               </td>
+          
+        
+                <td id="right">
+                    <?php
+                         echo "right";
+                     ?>  
+               </td>
+            </tr>            
+        </table>
+       
+    </body>
+</html>
